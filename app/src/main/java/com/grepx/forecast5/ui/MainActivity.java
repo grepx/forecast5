bce9558e0cd188ddab2e5f554779804fe89580d3
Greg Pearce <gregrpearce@gmail.com>
2016-11-01 11:02:42 +0000

    Add search box

--- app/src/main/java/com/grepx/forecast5/ui/MainActivity.java
+++ app/src/main/java/com/grepx/forecast5/ui/MainActivity.java
@@ -3,6 +3,10 @@ package com.grepx.forecast5.ui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import com.grepx.forecast5.R;
 import com.grepx.forecast5.domain.DayForecast;
 import com.grepx.forecast5.domain.ForecastService;
@@ -17,13 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
   ForecastService forecastService;
 
+  EditText searchEditText;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
     injectDependencies();
 
-    setContentView(R.layout.activity_main);
+    setupView();
 
     forecastService.forecast("London")
                    .subscribeOn(Schedulers.io())
@@ -42,5 +48,27 @@ public class MainActivity extends AppCompatActivity {
   private void injectDependencies() {
     // in a production architecture I'd use Dagger to do this
     forecastService = new ForecastServiceImpl();
+  }
+
+  private void setupView() {
+    setContentView(R.layout.activity_main);
+
+    searchEditText = (EditText) findViewById(R.id.search);
+
+    searchEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+      @Override
+      public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+            actionId == EditorInfo.IME_ACTION_DONE ||
+            event != null &&
+            event.getAction() == KeyEvent.ACTION_DOWN &&
+            event.getKeyCode() == KeyEvent.KEYCODE_ENTER
+            ) {
+          Log.d(TAG, "onEditorAction: " + searchEditText.getText());
+          return true;
+        }
+        return false;
+      }
+    });
   }
 }
