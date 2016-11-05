64a513d5f54ec3969289c21aff64b257ff9076f1
Greg Pearce <gregrpearce@gmail.com>
2016-11-05 17:20:20 +0000

    Add error unit test. Currently failing due to dependency on Log.e in presenter

--- app/src/test/java/com/grepx/forecast5/domain/MainPresenterTest.java
+++ app/src/test/java/com/grepx/forecast5/domain/MainPresenterTest.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.threeten.bp.DayOfWeek;
@@ -12,6 +13,9 @@ import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.ZoneOffset;
 import rx.Observable;
 import rx.schedulers.Schedulers;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
 
 public class MainPresenterTest {
 
@@ -22,17 +26,13 @@ public class MainPresenterTest {
 
   @Test
   public void doSearch_success() throws Exception {
+    // configure test data
     final String placeName = "London";
 
-    MainView mainView = Mockito.mock(MainView.class);
-    ForecastService forecastService = Mockito.mock(ForecastService.class);
-    SubscriptionConfig subscriptionConfig = new SubscriptionConfig(Schedulers.immediate(),
-                                                                   Schedulers.immediate());
-
     List<HourForecast> hourForecasts = new ArrayList<>();
-    hourForecasts.add(new HourForecast(LocalDateTime.ofEpochSecond(1000, 0, ZoneOffset.UTC),
+    hourForecasts.add(new HourForecast(LocalDateTime.ofEpochSecond(0, 0, ZoneOffset.UTC),
                                        100,
-                                       "http://google.com/logo",
+                                       "http://grepx.com/test",
                                        12.0f,
                                        5.5f,
                                        15.5f));
@@ -42,12 +42,47 @@ public class MainPresenterTest {
 
     Observable<List<DayForecast>> serviceResult = Observable.just(dayForecasts);
 
+    // configure mocks
+    MainView mainView = Mockito.mock(MainView.class);
+
+    ForecastService forecastService = Mockito.mock(ForecastService.class);
     Mockito.when(forecastService.forecast(placeName)).thenReturn(serviceResult);
 
+    // do test
+    SubscriptionConfig subscriptionConfig = new SubscriptionConfig(Schedulers.immediate(),
+                                                                   Schedulers.immediate());
     MainPresenter mainPresenter = new MainPresenter(mainView, forecastService, subscriptionConfig);
     mainPresenter.doSearch(placeName);
 
+    // assertions
     Mockito.verify(mainView).showResults(dayForecasts);
     Mockito.verify(mainView).showLoading(false);
+    Mockito.verify(mainView, never()).showError();
+  }
+
+  @Test
+  public void doSearch_error() throws Exception {
+    // configure test data
+    final String placeName = "London";
+
+    // service throws an exception
+    Observable<List<DayForecast>> serviceResult = Observable.error(new RuntimeException());
+
+    // configure mocks
+    MainView mainView = Mockito.mock(MainView.class);
+
+    ForecastService forecastService = Mockito.mock(ForecastService.class);
+    Mockito.when(forecastService.forecast(placeName)).thenReturn(serviceResult);
+
+    // do test
+    SubscriptionConfig subscriptionConfig = new SubscriptionConfig(Schedulers.immediate(),
+                                                                   Schedulers.immediate());
+    MainPresenter mainPresenter = new MainPresenter(mainView, forecastService, subscriptionConfig);
+    mainPresenter.doSearch(placeName);
+
+    // assertions
+    Mockito.verify(mainView, never()).showResults(ArgumentMatchers.<DayForecast>anyList());
+    Mockito.verify(mainView).showLoading(false);
+    Mockito.verify(mainView).showError();
   }
 }
